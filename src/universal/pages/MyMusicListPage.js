b24f5e0f90c6ceb6ecd57e687b04f1d2abcffeb2
Igor <igor.budzin95@gmail.com>
2018-11-08 17:07:43 +0200

    tmp

--- src/universal/pages/MyMusicListPage.js
+++ src/universal/pages/MyMusicListPage.js
@@ -1,15 +1,15 @@
 // Libraries
 import React, { Component, Fragment } from 'react';
 
-// import MyMusicListContainer from 'universal/containers/MyMusicListContainer';
+import MyMusicListContainer from 'universal/containers/MyMusicListContainer';
 import Header from 'universal/components/Header/Header';
 
 class MyMusicListPage extends Component {
-	render () {
+	render() {
 		return (
 			<div className="wrapper">
 				<Header />
-				{/*<MyMusicListContainer />*/}
+				<MyMusicListContainer />
 			</div>
 		);
 	}

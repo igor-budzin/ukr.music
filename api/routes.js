70c4ca5bda02d84b6131107793f471b94a6ebe68
Igor <igor.budzin95@gmail.com>
2018-11-30 17:53:38 +0200

    get files

--- api/routes.js
+++ api/routes.js
@@ -1,39 +1,63 @@
 const MysqlDBConnection = require('./MysqlDBConnection');
-const multer = require('multer');
+// const multer = require('multer');
+const multipart = require('connect-multiparty');
 const NodeID3 = require('node-id3');
 const Editor = require('id3-editor');
 const fs = require('fs');
 
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'files/audio')
-	},
-	filename: (req, file, cb) => {
-		let fileName = Date.now() + '_' + file.originalname;
+const multipartMiddleware = multipart();
+const filesPath = '/../files/audio/';
 
-		const connection = MysqlDBConnection()();
-		const sql = `INSERT INTO audio (link) VALUES ('${fileName}')`;
-
-		connection.query(sql, (error, results, fields) => {
-			if (error) { console.log(error) }
-			connection.destroy();
-		});
-
-		cb(null, fileName);
-		
-
-	}
-});
-const upload = multer({storage: storage});
 
 module.exports = (app, router) => {
 
-	router.post('/upload-music', upload.array('files'), (req, res, next) => {
-		res.send('OK');
-		// console.log(req);
-		// res.send('q111111')
+	router.post('/upload-music', multipartMiddleware, (req, res, next) => {
+		const filesArr = [];
+
+		if(req.files.files.length !== undefined) {
+			req.files.files.map((item) => {
+				filesArr.push(item);
+			});
+		}
+		else {
+			filesArr.push(req.files['files']);
+		}
+
+		let promises = filesArr.map((item) => {
+
+			return new Promise((resolve, reject) => {
+				fs.readFile(item.path, (err, data) => {
+					if (err) throw err;
+
+					const buffer = Buffer.from(data);
+					const fileName = Date.now() + '_' + item.originalFilename.replace(/ /g, '_');
+
+					fs.writeFile(__dirname + filesPath + fileName, buffer, (err) => {
+						if (err) throw err;
+						
+						console.log('ok')
+					});
+				});
+			});
+		});
+
+		Promise.all(promises).then(values => {
+			setTimeout(() => {
+				console.log("the end");
+				res.send(JSON.stringify({status: "ok"}));
+			}, 5000);
+		});
+
 		
 	});
+
+
+
+
+
+
+
+
 
 	router.get('/get-music', (req, res, next) => {
 		const connection = MysqlDBConnection()();

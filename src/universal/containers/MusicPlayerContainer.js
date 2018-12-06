ee663164534c778b3fd78d4c481c51b702e1732a
Igor <igor.budzin95@gmail.com>
2018-12-06 16:39:03 +0200

    player UI

--- src/universal/containers/MusicPlayerContainer.js
+++ src/universal/containers/MusicPlayerContainer.js
@@ -2,6 +2,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Slider from 'react-rangeslider';
+import { formatSeconds } from 'universal/utils';
+
 // Components
 // import MusicFilter from 'universal/components/MusicFilter';
 // import PlayList from 'universal/components/PlayList/PlayList';
@@ -11,15 +14,51 @@ import { bindActionCreators } from 'redux';
 
 @connect(mapStateToProps, mapDispatchToProps)
 class MusicPlayerContainer extends Component {
-	
+	constructor (props, context) {
+		super(props, context)
+
+		this.state = {
+			durationTime: 400,
+			currentTime: 0
+		}
+	}
+
+	handleChange = (currentTime) => {
+		this.setState({
+			currentTime: currentTime
+		})
+	}
+
 
 	render() {
 		return (
 			<div className="player">
-				<div className="controls">
+				<div className="controls controls--play">
 					<div className="btn prev"></div>
-					<div className="btn play-control"></div>
+					<div className="btn play"></div>
 					<div className="btn next"></div>
+				</div>
+
+				<div className="progress-bar">
+					<div className="time">
+						<span className="current">{formatSeconds(this.state.currentTime)}</span>
+						<span> / </span>
+						<span className="duration">{formatSeconds(this.state.durationTime)}</span>
+					</div>
+					<Slider
+						tooltip={false}
+						min={0}
+						max={this.state.durationTime}
+						step={0.2}
+						value={this.state.currentTime}
+						onChange={this.handleChange}
+					/>
+				</div>
+
+				<div className="controls controls--more">
+					<div className="btn repeat"></div>
+					<div className="btn shuffle"></div>
+					<div className="btn volume"></div>
 				</div>
 			</div>
 		);
--- src/universal/utils.js
+++ src/universal/utils.js
@@ -12,3 +12,24 @@ export function formatBytes(bytes, decimals) {
 		i = Math.floor(Math.log(bytes) / Math.log(k));
 	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
+
+export function formatSeconds(seconds) {
+	let string = '';
+	let sec_num = parseInt(seconds, 10);
+	let hours   = Math.floor(sec_num / 3600);
+	let mins = Math.floor((sec_num - (hours * 3600)) / 60);
+	let secs = sec_num - (hours * 3600) - (mins * 60);
+
+	if(hours > 0) {
+		if(hours < 10) string += hours + ':';
+		else string += '0' + hours + ':';
+	}
+	
+	if (mins < 10) string += "0" + mins + ':';
+	else string += mins + ':';
+
+	if (secs < 10) string += "0" + secs;
+	else string += secs;
+
+	return string;
+}

0cae259087d4a01bfe06c44b8b08d41a7a8d3cd1
Ігор <igor.budzin95@gmail.com>
2019-06-09 15:26:22 +0300

    tmp

--- src/universal/components/SearchField.js
+++ src/universal/components/SearchField.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
+import classNames from 'classnames';
 
 import 'universal/assets/styles/commons/search.scss';
 
@@ -18,6 +18,10 @@ const mapDispatchToProps = dispatch => {
 export default class SearchField extends Component {
   constructor(props, context) {
     super(props, context)
+
+    this.state = {
+      activeIndex: null
+    }
   }
 
   componentDidMount() {
@@ -29,72 +33,87 @@ export default class SearchField extends Component {
   }
 
   keyControl = event => {
-    let searchItems = Array.prototype.slice.call(document.getElementsByClassName('search-item'));
-    let activeIndex = 0;
-
-    searchItems.map((item, index) => {
-      if(item.classList.contains('active')) activeIndex = index;
-    });
+    const { activeIndex } = this.state;
 
     switch(event.keyCode) {
-      case 38:
-
-      break;
-
-      case 40:
-
-        searchItems[activeIndex].classList.remove('active');
-        if(activeIndex <= searchItems.length) {
-          searchItems[++activeIndex].classList.add('active');
+       case 38: // Arrow Up
+        if(activeIndex === null || activeIndex - 1 < 0) {
+          this.setState({ activeIndex: data.length - 1 });
         }
-      break;
+        else if(activeIndex - 1 >= 0) {
+          this.setState({ activeIndex: activeIndex - 1 });
+        }
+
+       break;
+
+       case 40: // Arrow Down
+        if(activeIndex === null || activeIndex + 1 > data.length - 1) {
+          this.setState({ activeIndex: 0 });
+        }
+        else if(activeIndex + 1 <= data.length - 1) {
+          this.setState({ activeIndex: activeIndex + 1 });
+        }
+       break;
+
+       case 13:
+        if(activeIndex !== null) {
+
+        }
+       break;
     }
-    console.log(activeIndex)
+  }
+
+  handleChangeInput = () => {
+    this.setState({ activeIndex: null });
   }
 
   render() {
     return (
       <div className="search-wrapper">
-        <input type="text" className="search-input" placeholder="Пошук..." />
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Пошук..."
+          onChange={this.handleChangeInput}
+        />
 
         <div className="autocomplete">
-          <div className="search-item audio">
-            <div className="cover"></div>
-            <div className="description">
-              <div className="title">The Hardkiss</div>
-              <div className="sub-title">Трек виконавця <b>The Hardkiss</b></div>
-            </div>
-          </div>
-
-          <div className="search-item audio">
-            <div className="cover"></div>
-            <div className="description">
-              <div className="title">The Hardkiss</div>
-              <div className="sub-title">Трек виконавця <b>The Hardkiss</b></div>
-            </div>
-          </div>
-
-          <div className="search-item artist">
-            <div className="cover"></div>
-            <div className="description">
-              <div className="title">The Hardkiss</div>
-              <div className="sub-title">Виконавець</div>
-            </div>
-          </div>
-
-          <div className="search-item album">
-            <div className="cover"></div>
-            <div className="description">
-              <div className="title">Залізна ластівка</div>
-              <div className="sub-title">Альбом виконавця <b>The Hardkiss</b></div>
-            </div>
-          </div>
-
-          <div className="search-item search">
-            <div className="title">Залізна ластівка</div>
-          </div>
+          {data.length && data.map((item, index) => {
+            return(
+              <SearchItem
+                key={index}
+                item={item}
+                active={this.state.activeIndex === index}
+              />
+            )
+          })}
         </div>
       </div>
     );
   }
 }
+
+const SearchItem = props => {
+  return (
+    <div className={classNames(
+      'search-item',
+      props.item.type,
+      { active: props.active }
+    )}>
+      { props.item.type !== 'search' && <div className="cover"></div> }
+
+      <div className="description">
+        <div className="title">{props.item.title}</div>
+        { props.item.type !== 'search' && <div className="sub-title">{props.item.subtitle}</div> }
+      </div>
+    </div>
+  )
+}
+
+const data = [
+  { type: 'audio', title: 'The Hardkiss', 'subtitle': 'Трек виконавця The Hardkiss'},
+  { type: 'audio', title: 'The Hardkiss', 'subtitle': 'Трек виконавця The Hardkiss'},
+  { type: 'album', title: 'The Hardkiss', 'subtitle': 'Альбом виконавця The Hardkiss'},
+  { type: 'artist', title: 'The Hardkiss', 'subtitle': 'Виконавець'},
+  { type: 'search', title: 'Залізна ластівка' }
+];
